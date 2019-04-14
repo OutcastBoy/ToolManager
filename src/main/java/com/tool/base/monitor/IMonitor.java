5eca08359bebb99e88d614a6ce0f2987c925a65c
OutcastBoy <warptenxie@qq.com>
2019-04-14 22:03:22 +0800

    Initial commit

---
+++ src/main/java/com/tool/base/monitor/IMonitor.java
@@ -0,0 +1,11 @@
+package com.tool.base.monitor;
+
+/**
+ * @author qiup
+ * @description 查询状态基础接口
+ * @date 2019/4/14
+ */
+public interface IMonitor {
+
+    public  String getInfo();
+}
---
+++ src/main/java/com/tool/tomcat/monitor/TomCatMonitor.java
@@ -0,0 +1,17 @@
+package com.tool.tomcat.monitor;
+
+import com.tool.base.monitor.IMonitor;
+
+/**
+ * @author qiup
+ * @description tomcat监控实现类
+ * @date 2019/4/14
+ */
+public class TomCatMonitor implements IMonitor {
+
+
+    @Override
+    public String getInfo() {
+        return null;
+    }
+}
